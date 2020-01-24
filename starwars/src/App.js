6e5aca14a5643a5c98a084f976d778b8a87dc118
Restyled.io <commits@restyled.io>
2020-01-24 18:54:31 +0000

    Restyled by prettier

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,35 +1,39 @@
-import './App.css';
+import "./App.css";
 
 import axios from "axios";
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-import CharacterCard from './CharacterCard';
+import CharacterCard from "./CharacterCard";
 
 export default function CardInfo() {
   const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-      axios.get('https://swapi.co/api/people/?page=1')
+    axios
+      .get("https://swapi.co/api/people/?page=1")
       .then(response => {
-          console.log(response.data.results)
-          setCharacter(response.data.results)
+        console.log(response.data.results);
+        setCharacter(response.data.results);
       })
       .catch(err => {
-    console.log('no data', err);
-      })
+        console.log("no data", err);
+      });
   }, []);
 
   return (
-      <div>
-          {character.map((person, index) => {
-      return < CharacterCard
-              key={index}
-              name={person.name}
-              birth={person.birth_year}
-              height={person.height}
-              hair={person.hair_color}
-              eyes={person.eye_color} />
-          })}
-      </div>
-  )
+    <div>
+      {character.map((person, index) => {
+        return (
+          <CharacterCard
+            key={index}
+            name={person.name}
+            birth={person.birth_year}
+            height={person.height}
+            hair={person.hair_color}
+            eyes={person.eye_color}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const WrapperDiv = styled.div`
-width: 100%;
-display: flex;
+  width: 100%;
+  display: flex;
 
-align-items: center;
+  align-items: center;
 `;
 const Firstdiv = styled.div`
-width:45%;
+  width: 45%;
 `;
 const Seconddiv = styled.div`
-width:45%;
+  width: 45%;
 `;
 
-const CharacterCard =
-    (props) => {
-      const {name, birth, height, hair, eyes} = props;
+const CharacterCard = props => {
+  const { name, birth, height, hair, eyes } = props;
 
-      return (<WrapperDiv><Firstdiv><h2>{name}</h2>
-        </Firstdiv>
-              <Seconddiv>
-              <p>Born: {birth}</p>
-            <p>Height: {height}</p>
-              <p>Hair Color: {hair}</p>
-            <p>Eye Color: {eyes}</p>
-              </Seconddiv>
-        </WrapperDiv>)
-    }
+  return (
+    <WrapperDiv>
+      <Firstdiv>
+        <h2>{name}</h2>
+      </Firstdiv>
+      <Seconddiv>
+        <p>Born: {birth}</p>
+        <p>Height: {height}</p>
+        <p>Hair Color: {hair}</p>
+        <p>Eye Color: {eyes}</p>
+      </Seconddiv>
+    </WrapperDiv>
+  );
+};
 
 export default CharacterCard;
