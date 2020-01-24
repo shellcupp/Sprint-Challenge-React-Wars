8e3f13be5585340fa7be313908304532ada414d4
Michelle Cupp <shellcupp@gmail.com>
2020-01-24 12:49:31 -0600

    meets mvp i think

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const WrapperDiv = styled.div`
+width: 100%;
+display: flex;
+
+align-items: center;
+`;
+const Firstdiv = styled.div`
+width:45%;
+`;
+const Seconddiv = styled.div`
+width:45%;
+`;
 
 const CharacterCard = (props) => {
     const {name, birth, height, hair, eyes} = props;
 
     return (
-        <div>
+        <WrapperDiv>
+        <Firstdiv>
             <h2>{name}</h2>
+        </Firstdiv>
+        <Seconddiv>
             <p>Born: {birth}</p>
             <p>Height: {height}</p>
             <p>Hair Color: {hair}</p>
             <p>Eye Color: {eyes}</p>
-        </div>
+        </Seconddiv>
+        </WrapperDiv>
     )
 }
 
